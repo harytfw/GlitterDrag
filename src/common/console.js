aeb77a064ee641f799b9274bae3eebb6cd54dfb5
harytfw <harytfw@outlook.com>
2020-07-19 13:21:09 +0800

    增加从外界获取参数能力

--- src/common/console.js
+++ src/common/console.js
@@ -1,48 +1,54 @@
 "use strict";
 var consoleUtil = {};
 {
+    function getFlag(key, def) {
+        if (key in globalThis) {
+            return globalThis[key]
+        }
+        return def
+    }
+
     const F_LOG = true;
     const F_DEBUG = true;
     const F_INFO = false;
     const F_WARN = true;
     const F_ERROR = true;
     const F_TRACE = true;
-    const F_HIDE = true;
 
     consoleUtil.autoHide = true;
 
     consoleUtil.log = (...args) => {
-        if (F_LOG) {
+        if (getFlag("F_LOG", F_LOG)) {
             console.log(...args);
         }
     };
 
     consoleUtil.debug = (...args) => {
-        if (F_DEBUG) {
+        if (getFlag("F_DEBUG", F_DEBUG)) {
             console.debug(...args);
         }
     };
 
     consoleUtil.info = (...args) => {
-        if (F_INFO) {
+        if (getFlag("F_INFO", F_INFO)) {
             console.info(...args);
         }
     };
 
     consoleUtil.error = (...args) => {
-        if (F_ERROR) {
+        if (getFlag("F_ERROR", F_ERROR)) {
             console.error(...args);
         }
     };
 
     consoleUtil.warn = (...args) => {
-        if (F_WARN) {
+        if (getFlag("F_WARN", F_WARN)) {
             console.warn(...args);
         }
     };
 
     consoleUtil.trace = (...args) => {
-        if (F_TRACE) {
+        if (getFlag("F_TRACE", F_TRACE)) {
             console.trace(...args);
         }
     };
